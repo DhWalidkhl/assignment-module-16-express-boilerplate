1d2bcddfd6a862184850f4008760af5563e65f06
DhWalidkhl <sm.walid69@yahoo.com>
2024-11-04 23:00:22 +0600

    upload empty file

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,16 @@ const router = express.Router()
 import * as blogController from "../app/controllers/blogController.js";
 
 
-// implement assignment router here
+// Create Route
 router.post('/create-blog', blogController.createBlog)
+
+// Read Route
 router.get('/read-blog', blogController.readBlog)
+
+// Update Route
 router.put('/update-blog', blogController.updateBlog)
+
+// Delete Route
 router.delete('/delete-blog', blogController.deleteBlog)
 
 
